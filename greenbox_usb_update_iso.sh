4b9705c7b6d1d55e9855060984f39a03aa1a6f1b
hernad <hernad@bring.out.ba>
2017-05-24 10:06:05 +0200

    fix console

--- greenbox_usb_update_iso.sh
+++ greenbox_usb_update_iso.sh
@@ -3,15 +3,16 @@
 # https://github.com/bjasko/docker_scripts/blob/master/greenbox_usb_update
 
 
-if [ $# -lt 2 ]
+if [ $# -lt 3 ]
 then
-    echo "argumenti su:   $0 <HOST> <GREENBOX_VER> [--restart]"
-    echo " npr:   $0 192.168.168.110 3.7.2"
+    echo "argumenti su:   $0 <HOST> <GREENBOX_VER> <CONSOLE> [--restart]"
+    echo " npr:   $0 192.168.168.110 3.7.2 vga --restart"
     exit 1
 fi
 
 HOST=$1
 GREENBOX_VER=$2
+CONSOLE=$3
 
 if [ "$3" == "--restart" ] || [ "$3" == "--reboot" ] ; then
   REBOOT=1
@@ -33,7 +34,7 @@ if [ -z "$SCRIPT" ] ; then
 
 fi
 
-CMD="sudo /usr/local/bin/greenbox_usb_update $GREENBOX_VER"
+CMD="sudo /usr/local/bin/greenbox_usb_update $GREENBOX_VER $CONSOLE"
 echo $CMD
 ssh docker@$HOST "$CMD" 
 
