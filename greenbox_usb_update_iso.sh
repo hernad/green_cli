e418d5b8c15be8009285213b081e9848356a8421
hernad <hernad@bring.out.ba>
2017-05-24 09:02:11 +0200

    iso update

--- greenbox_usb_update_iso.sh
+++ greenbox_usb_update_iso.sh
@@ -3,74 +3,41 @@
 # https://github.com/bjasko/docker_scripts/blob/master/greenbox_usb_update
 
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-    echo "argumenti su:   ./greenbox_usb_update  [GREENBOX_VER]"
-    echo " npr:   ./greenbox_usb_create 3.7.2"
+    echo "argumenti su:   $0 <HOST> <GREENBOX_VER> [--restart]"
+    echo " npr:   $0 192.168.168.110 3.7.2"
     exit 1
 fi
 
-if [ "$(id -u)" != "0" ]; then
-    echo "Pokreni kao root" 1>&2
-    exit 1
-fi
+HOST=$1
+GREENBOX_VER=$2
 
-GREENBOX_VERSION=$1
-GREEN_ISO=http://download.bring.out.ba/greenbox-${GREENBOX_VERSION}.iso
-GREEN_CFG=/media/syslinux.cfg
-DEF_BOOT=`cat $GREEN_CFG | grep "^default boot" | awk '{print $2}'`
-ISO_MNT=/tmp/$1
-BOOT_PATH=/media/boot
-
-
-if wget --spider $GREEN_ISO 2>/dev/null; then
-    echo "update postoji, nastavljam"
-    cd  /tmp && wget -q $GREEN_ISO
-    
-else
-    echo "update iso ne postoji"
-    exit
+if [ "$3" == "--restart" ] || [ "$3" == "--reboot" ] ; then
+  REBOOT=1
 fi
 
 
-echo "defaultni boot je $DEF_BOOT"
+echo start
+SCRIPT=`ssh docker@$HOST  ls /usr/local/bin/greenbox_usb_update`
 
-if [ -d "$ISO_MNT" ]; then
-    echo "ISO mountpoint $ISO_MNT postoji, provjeritte, prekidam..... "
-    exit
-else
-    mkdir $ISO_MNT
-    mount -o loop /tmp/greenbox-$1.iso  $ISO_MNT
+echo $SCRIPT
+
+if [ -z "$SCRIPT" ] ; then
+  echo "install greenbox_usb_update script on host" 
+  scp greenbox_usb_update.script  docker@${HOST}:/tmp/greenbox_usb_update &&\
+  ssh docker@${HOST} "sudo mv /tmp/greenbox_usb_update /usr/local/bin/ ; chmod +x /usr/local/bin/greenbox_usb_update"
+
+#else
+#  ssh docker@$HOST  cat /usr/local/bin/greenbox_usb_update
+
 fi
 
-if [ -d "$BOOT_PATH/$1" ]; then
-    echo "$BOOT_PATH/$1 već postoji, provjerite......"
-    exit
-else
-    mkdir $BOOT_PATH/$1
-    cp $ISO_MNT/boot/initrd.img  $BOOT_PATH/$1
-    cp $ISO_MNT/boot/vmlinuz64 $BOOT_PATH/$1
-    umount $ISO_MNT
-    rm -rf $ISO_MNT
-fi
+CMD="sudo /usr/local/bin/greenbox_usb_update $GREENBOX_VER"
+echo $CMD
+ssh docker@$HOST "$CMD" 
 
-
-if [ "$DEF_BOOT" == "boot$1" ]; then
-    echo "$DEF_BOOT je već defaultni boot ...prekidam"
-    exit
-else
-    echo "label boot$1" >> $GREEN_CFG
-    echo "menu label greenbox-$1" >> $GREEN_CFG
-    echo "kernel /boot/$1/vmlinuz64"  >> $GREEN_CFG
-    echo "append initrd=/boot/$1/initrd.img loglevel=3 dockerpwd=test01 nozswap nofstab tz=CET-1CEST,M3.5.0,M10.5.0/3 noembed nomodeset norestore waitusb=10 LABEL=GREEN_HDD console=ttyS0,115200n8"  >> $GREEN_CFG
-    echo ""  >> $GREEN_CFG
-fi
-
-if  [ "$DEF_BOOT" == "" ]; then
-    echo "$DEF_BOOT je prazan dodajem ga"
-    sed -i '3idefault boot'"$1"'\'  $GREEN_CFG
-    sync
-else
-    sed -i -e 's/default '"$DEF_BOOT"'/default boot'"$1"'/' $GREEN_CFG
-    sync
+if [ -n "$REBOOT" ] ; then
+   echo "rebooting $HOST ..."
+   ssh docker@$HOST "sudo reboot -d 2 -f"
 fi
